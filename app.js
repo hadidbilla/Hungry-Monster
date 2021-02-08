542440674677b7aeff61a433c7258ed3e2c63998
hbhridoy449 <50765387+hbhridoy449@users.noreply.github.com>
2021-02-08 12:44:36 +0600

    change in css file

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const displayFood = (data, inputText) => {
     data.meals == null
   ) {
     let showData = `
-      <h1 class="p-5 text-center">
+      <h1 style="color: #f06c4e;" class="p-5 text-center">
       Please search Valid Meals Name !
       </h1>
         `;
@@ -24,12 +24,12 @@ const displayFood = (data, inputText) => {
   } else {
     data.meals.forEach((meal) => {
       let foodContainerDiv = document.createElement("div");
-      foodContainerDiv.className = "col-4  p-5 item rounded-3";
+      foodContainerDiv.className = "col-4  p-5 rounded-3";
       const foodInfo = `
-      <div onClick="mealsInfo('${meal.idMeal}')" class="card" style="width: 18rem;">
+      <div onClick="mealsInfo('${meal.idMeal}')" class="card item p-4 shadow-lg items" style="width: 18rem;">
         <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
-            <p class="card-text">${meal.strMeal}</p>
+            <p class="card-text text-center"><strong>${meal.strMeal}</strong></p>
         </div>
     </div>
     
@@ -37,8 +37,8 @@ const displayFood = (data, inputText) => {
       foodContainerDiv.innerHTML = foodInfo;
       showFood.appendChild(foodContainerDiv);
     });
-    let searchFood = (document.getElementById("input-text").value = "");
   }
+  let searchFood = (document.getElementById("input-text").value = "");
 };
 const mealsInfo = (id) => {
   const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
@@ -58,4 +58,5 @@ const renderMealInfo = (data) => {
   document.getElementById("strIngredient5").innerText = data.strIngredient5;
   document.getElementById("strMeasure1").innerText = data.strMeasure1;
   document.getElementById("strMeal").innerText = data.strMeal;
+  document.getElementById("strMeasure3").innerText = data.strMeasure3;
 };
